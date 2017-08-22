b00bcf263b03595d4b03b7c1d1e976bfd721fb85
Jefferson Mariano de Souza <jefferson.souza@synchro.com.br>
2017-08-22 12:41:02 -0300

    Enviando dados para um serviço web

--- src/main/java/br/com/jmsstudio/camel/RotaPedidos.java
+++ src/main/java/br/com/jmsstudio/camel/RotaPedidos.java
@@ -3,6 +3,7 @@ package br.com.jmsstudio.camel;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class RotaPedidos {
@@ -14,16 +15,20 @@ public class RotaPedidos {
 		context.addRoutes(new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                from("file://pedidos?noop=true")
+                from("file://pedidos?delay=5s&noop=true")
+                    .setProperty("pedidoId", xpath("/pedido/id"))
+                    .setProperty("clientId", xpath("/pedido/pagamento/email-titular"))
                     .split()
                         .xpath("/pedido/itens/item")
                     .filter()
                         .xpath("/item/formato[text()='EBOOK']")
+                        .setProperty("ebookId", xpath("/item/livro/codigo"))
                     .marshal()
                         .xmljson()
-                    .log("${id} - ${body}")
-                .setHeader(Exchange.FILE_NAME, simple("${file:name.noext}-${header.CamelSplitIndex}.json"))
-                .to("file://saida");
+                        .log("${id} - ${body}")
+                .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.GET))
+                .setHeader(Exchange.HTTP_QUERY, simple("clienteId=${property.clientId}&pedidoId=${property.pedidoId}&ebookId=${property.ebookId}"))
+                .to("http4://localhost:8080/webservices/ebook/item");
             }
         });
 
